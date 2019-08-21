adee6e8609e22530958d85ee55d6d62f23e1937e
Thomas Roman <thomasroman32@yahoo.com>
2019-08-20 23:56:58 -0500

    added to the menuchoices array. and created inquirer prompt in supervisor function in supervisor.js fil

--- scripts/bamazonSupervisor.js
+++ scripts/bamazonSupervisor.js
@@ -7,7 +7,17 @@ let Supervisor = function(sql) {
     this.start = function (callback) {
         let that = this;
 
-        let menuChoices = [console(log)];
+        let menuChoices = [   
+        "v) View Product Sales by Department",
+        "n) Add New Department",
+        "e) Return to Main Menu"];
+
+        inquirer.prompt({
+            name: 'menuChoice',
+            type: 'list',
+            message: 'Welcome Supervisor what would you like to do?',
+            choices:menuChoices
+        })
     }
 };
 
