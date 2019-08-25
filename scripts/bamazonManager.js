7d1701666bf21f3dc66ec3a7452b463617fe267a
Thomas Roman <thomasroman32@yahoo.com>
2019-08-24 23:35:28 -0500

    created .start function and created array menuChoices with menu that displays on console.

--- scripts/bamazonManager.js
+++ scripts/bamazonManager.js
@@ -2,4 +2,17 @@ let mysql = require('mysql');
 let inquirer = require('inquirer');
 let Table =  require('cli-table');
 
-let Manager = function(sql) {}
+let Manager = function(sql) {var Manager = function(sql){
+    this.sql = sql;
+    this.start = function(callback){
+        let that = this;
+        let menuChoices = [
+            "i) View Current Inventory",
+            "l) View Low Inventory",
+            "a) Add to Inventory",
+            "n) Add New Product",
+            "e) Return to Main Menu"
+        ];
+    }
+  }
+};
