af89aaf7a4314eac94e233a54af43a7aea8888af
Thomas Roman <thomasroman32@yahoo.com>
2019-08-25 23:57:46 -0500

    added the inquirer prompt in the bamazonManager file, is currently not working but will update as soon as possible

--- scripts/bamazonManager.js
+++ scripts/bamazonManager.js
@@ -13,6 +13,29 @@ let Manager = function(sql) {var Manager = function(sql){
             "n) Add New Product",
             "e) Return to Main Menu"
         ];
+        inquirer.prompt({
+            name: 'menuChoice',
+            type: 'list',
+            message: 'Welcome Manager what would you like to do?',
+            choices:menuChoices
+        }).then(function(answer){
+            switch(answer.menuChoice.split(")")[0]){
+                case 'i':
+                    that.sql.query("SELECT * FROM products", function(err, res){
+                        if(err) throw err;
+                        var dbTable = new Table({
+                            head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Qty'],
+                            colWidths: [10, 50, 30, 12, 20]
+                        });
+                        res.forEach(function(product){
+                            dbTable.push([product.item_id, product.product_name, product.department_name, product.price, product.stock_quantity]);
+                        });
+
+                        console.log(dbTable.toString() + "\n");
+                        that.start(callback);
+                    });
+                    
+
     }
   }
 };
