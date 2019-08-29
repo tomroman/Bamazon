087271ec00d7ab2dc44069bdfd6b4fe23f91077b
Thomas Roman <thomasroman32@yahoo.com>
2019-08-29 08:41:03 -0500

    updating var to let in the bamazonCustomer file, and also adding an app.js file, and requiring as variables.

---
+++ app.js
@@ -0,0 +1,6 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var Customer = require('./scripts/bamazonCustomer.js');
+var Manager = require('./scripts/bamazonManager.js');
+var Supervisor = require('./scripts/bamazonSupervisor.js');
+var databaseKey = require('./database_key.js');
